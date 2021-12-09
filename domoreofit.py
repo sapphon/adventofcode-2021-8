49739e0c72050477ee6f5ca84e81395fe5fa9991
Connor Shaughnessy <sapphon@gmail.com>
2021-12-09 12:29:07 -0500

    Inlines and renames

--- domoreofit.py
+++ domoreofit.py
@@ -2,36 +2,35 @@ def setify(code):
     return frozenset([char for char in code])
 
 
-def infer_digit_by_comparing_to_1_and_4(mystery_code, one_code, four_code):
-    if len(mystery_code) == 5:
-        if setify(mystery_code).issuperset(setify(one_code)):
+def get_digit_by_comparing_to_1_and_4(mystery_set, one_code, four_code):
+    if len(mystery_set) == 5:
+        if mystery_set.issuperset(setify(one_code)):
             return 3
-        elif len(setify(four_code) - setify(mystery_code)) == 2:
+        elif len(setify(four_code) - mystery_set) == 2:
             return 2
         else:
             return 5
     else:
-        if setify(mystery_code).issuperset(setify(four_code)):
+        if mystery_set.issuperset(setify(four_code)):
             return 9
-        elif setify(mystery_code).issuperset(setify(one_code)):
+        elif mystery_set.issuperset(setify(one_code)):
             return 0
         else:
             return 6
 
 
-def get_number_codes(digit_codes):
-    number_codes = [''] * 10
+def get_digit_codes(digit_codes):
+    digits_to_codes = [''] * 10
     digit_codes.sort(key=len)
     for (digit, index) in [(1, 0), (7, 1), (4, 2), (8, 9)]:
-        number_codes[digit] = digit_codes[index]
-    the_remaining_codes = [digit_codes[x] for x in range(3, 9)]
-    for code in the_remaining_codes:
-        number_codes[infer_digit_by_comparing_to_1_and_4(code, number_codes[1], number_codes[4])] = code
-    return number_codes
+        digits_to_codes[digit] = digit_codes[index]
+    for code in [digit_codes[x] for x in range(3, 9)]:
+        digits_to_codes[get_digit_by_comparing_to_1_and_4(setify(code), digits_to_codes[1], digits_to_codes[4])] = code
+    return digits_to_codes
 
 
-def translate_output(output_codes, legend):
-    codes_to_digits = {setify(val): idx for idx, val in enumerate(legend)}
+def translate_output(output_codes, digits_to_codes):
+    codes_to_digits = {setify(val): idx for idx, val in enumerate(digits_to_codes)}
     output_digits = ''
     for code in output_codes:
         output_digits += str(codes_to_digits[setify(code)])
@@ -42,5 +41,5 @@ with open('input.txt') as incoming:
     sum_of_readouts = 0
     for line in incoming.readlines():
         (digit_string, output_string) = map(str.strip, line.split("|"))
-        sum_of_readouts += int(translate_output(output_string.split(" "), get_number_codes(digit_string.split(" "))))
+        sum_of_readouts += int(translate_output(output_string.split(" "), get_digit_codes(digit_string.split(" "))))
     print(sum_of_readouts)
