156243dfa28a01ca2d39ab150fcb18aacbe7b80d
Connor Shaughnessy <sapphon@gmail.com>
2021-12-08 21:18:34 -0500

    Use sorting to simplify locating unique-len digit codes

--- domoreofit.py
+++ domoreofit.py
@@ -21,8 +21,9 @@ def infer_number_by_comparing_to_1_and_4(mystery_code, one_code, four_code):
 
 def get_number_codes(digit_codes):
     number_codes = [''] * 10
-    for digit_and_length in [(1, 2), (7, 3), (4, 4), (8, 7)]:
-        number_codes[digit_and_length[0]] = next(x for x in digit_codes if len(x) == digit_and_length[1])
+    digit_codes.sort(key=len)
+    for (digit, index) in [(1, 0), (7, 1), (4, 2), (8, 9)]:
+        number_codes[digit] = digit_codes[index]
     the_remaining_codes = [x for x in digit_codes if len(x) == 5 or len(x) == 6]
     for code in the_remaining_codes:
         number_codes[infer_number_by_comparing_to_1_and_4(code, number_codes[1], number_codes[4])] = code
@@ -33,7 +34,7 @@ def translate_output(output_codes, legend):
     output_digits = ''
     for code in output_codes:
         for i in range(10):
-            if setify(legend[i]) == setify(code.rstrip()):
+            if setify(legend[i]) == setify(code):
                 output_digits += str(i)
     return output_digits
 
@@ -42,6 +43,6 @@ with open('input.txt') as incoming:
     lines_of_input = incoming.readlines()
     sum_of_readouts = 0
     for line in lines_of_input:
-        (digit_string, output_string) = line.split("|")
+        (digit_string, output_string) = map(str.strip, line.split("|"))
         sum_of_readouts += int(translate_output(output_string.split(" "), get_number_codes(digit_string.split(" "))))
     print(sum_of_readouts)
