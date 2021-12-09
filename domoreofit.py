96693dd1a4c543da409b10993ad9c2d0a8adb8d4
Connor Shaughnessy <sapphon@gmail.com>
2021-12-08 19:48:46 -0500

    Done with Part 2
    Also eliminate messy scope sharing

--- domoreofit.py
+++ domoreofit.py
@@ -1,40 +1,51 @@
-def get_number_codes(line):
-    number_codes = [''] * 10
-    def setify(code):
-        return set([char for char in code])
+def setify(code):
+    return set([char for char in code])
 
 
-    def infer_number_from_code(mystery_code):
-        if len(mystery_code) == 5:
-            if setify(mystery_code).issuperset(setify(number_codes[1])):
-                return 3
-            elif len(setify(number_codes[4]) - setify(mystery_code)) == 2:
-                return 2
-            else:
-                return 5
+def infer_number_by_comparing_to_1_and_4(mystery_code, one_code, four_code):
+    if len(mystery_code) == 5:
+        if setify(mystery_code).issuperset(setify(one_code)):
+            return 3
+        elif len(setify(four_code) - setify(mystery_code)) == 2:
+            return 2
         else:
-            if setify(mystery_code).issuperset(setify(number_codes[4])):
-                return 9
-            elif setify(mystery_code).issuperset(setify(number_codes[1])):
-                return 0
-            else:
-                return 6
+            return 5
+    else:
+        if setify(mystery_code).issuperset(setify(four_code)):
+            return 9
+        elif setify(mystery_code).issuperset(setify(one_code)):
+            return 0
+        else:
+            return 6
 
-    (digits, outputs) = line.split("|")
-    digits = digits.split(" ")
-    outputs = outputs.split(" ")
-    number_codes[1] = next(x for x in digits if len(x) == 2)
-    number_codes[7] = next(x for x in digits if len(x) == 3)
-    number_codes[4] = next(x for x in digits if len(x) == 4)
-    number_codes[8] = next(x for x in digits if len(x) == 7)
-    the_remaining_codes = [x for x in digits if len(x) == 5 or len(x) == 6]
+
+def get_number_codes(digit_codes):
+    number_codes = [''] * 10
+
+    number_codes[1] = next(x for x in digit_codes if len(x) == 2)
+    number_codes[7] = next(x for x in digit_codes if len(x) == 3)
+    number_codes[4] = next(x for x in digit_codes if len(x) == 4)
+    number_codes[8] = next(x for x in digit_codes if len(x) == 7)
+    the_remaining_codes = [x for x in digit_codes if len(x) == 5 or len(x) == 6]
     for code in the_remaining_codes:
-        number_codes[infer_number_from_code(code)] = code
-
+        number_codes[infer_number_by_comparing_to_1_and_4(code, number_codes[1], number_codes[4])] = code
     return number_codes
 
 
+def translate_output(output_codes, legend):
+    output_digits = ''
+    for code in output_codes:
+        for i in range(10):
+            if setify(legend[i]) == setify(code.rstrip()):
+                output_digits += str(i)
+    return output_digits
+
+
 with open('input.txt') as incoming:
-    lines = incoming.readlines()
-    for line in lines:
-        print(get_number_codes(line))
+    lines_of_input = incoming.readlines()
+    sum_of_readouts = 0
+    for line in lines_of_input:
+        (digit_string, output_string) = line.split("|")
+        sum_of_readouts += int(translate_output(output_string.split(" "), get_number_codes(digit_string.split(" "))))
+    print(sum_of_readouts)
+
