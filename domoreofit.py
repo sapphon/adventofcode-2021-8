2abc3f572176979092c7357abd2a0ad62f272e46
Connor Shaughnessy <sapphon@gmail.com>
2021-12-13 12:48:33 -0500

    Sort the input strings as they come in

--- domoreofit.py
+++ domoreofit.py
@@ -17,9 +17,9 @@ def get_digit_codes(digit_codes):
     digits_to_codes = [''] * 10
     digit_codes.sort(key=len)
     for (digit, index) in [(1, 0), (7, 1), (4, 2), (8, 9)]:
-        digits_to_codes[digit] = sort_string(digit_codes[index])
+        digits_to_codes[digit] = digit_codes[index]
     for code in [digit_codes[x] for x in range(3, 9)]:
-        digits_to_codes[get_digit_by_comparing_to_1_and_4(setify(code), digits_to_codes[1], digits_to_codes[4])] = sort_string(code)
+        digits_to_codes[get_digit_by_comparing_to_1_and_4(setify(code), digits_to_codes[1], digits_to_codes[4])] = code
     return digits_to_codes
 
 
@@ -27,7 +27,7 @@ def translate_output(output_codes, digits_to_codes):
     codes_to_digits = {val: idx for idx, val in enumerate(digits_to_codes)}
     output_digits = ''
     for code in output_codes:
-        output_digits += str(codes_to_digits[sort_string(code)])
+        output_digits += str(codes_to_digits[code])
     return output_digits
 
 
@@ -35,5 +35,5 @@ with open('input.txt') as incoming:
     sum_of_readouts = 0
     for line in incoming.readlines():
         (digit_string, output_string) = map(str.strip, line.split("|"))
-        sum_of_readouts += int(translate_output(output_string.split(" "), get_digit_codes(digit_string.split(" "))))
+        sum_of_readouts += int(translate_output(map(sort_string, output_string.split(" ")), get_digit_codes(map(sort_string, digit_string.split(" ")))))
     print(sum_of_readouts)
