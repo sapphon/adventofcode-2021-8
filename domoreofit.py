aa9e398f9dcc68b032ca465a5f8ab1f563daca34
Connor Shaughnessy <sapphon@gmail.com>
2021-12-13 12:42:07 -0500

    Sets replaced with sorting everywhere except algo method

--- domoreofit.py
+++ domoreofit.py
@@ -2,6 +2,10 @@ def setify(code):
     return frozenset([char for char in code])
 
 
+def sort_string(string):
+    return ''.join(sorted(string))
+
+
 def get_digit_by_comparing_to_1_and_4(mystery_set, one_code, four_code):
     if len(mystery_set) == 5:
         return 3 if mystery_set.issuperset(setify(one_code)) else 2 if len(setify(four_code) - mystery_set) == 2 else 5
@@ -13,17 +17,17 @@ def get_digit_codes(digit_codes):
     digits_to_codes = [''] * 10
     digit_codes.sort(key=len)
     for (digit, index) in [(1, 0), (7, 1), (4, 2), (8, 9)]:
-        digits_to_codes[digit] = digit_codes[index]
+        digits_to_codes[digit] = sort_string(digit_codes[index])
     for code in [digit_codes[x] for x in range(3, 9)]:
-        digits_to_codes[get_digit_by_comparing_to_1_and_4(setify(code), digits_to_codes[1], digits_to_codes[4])] = code
+        digits_to_codes[get_digit_by_comparing_to_1_and_4(setify(code), digits_to_codes[1], digits_to_codes[4])] = sort_string(code)
     return digits_to_codes
 
 
 def translate_output(output_codes, digits_to_codes):
-    codes_to_digits = {setify(val): idx for idx, val in enumerate(digits_to_codes)}
+    codes_to_digits = {val: idx for idx, val in enumerate(digits_to_codes)}
     output_digits = ''
     for code in output_codes:
-        output_digits += str(codes_to_digits[setify(code)])
+        output_digits += str(codes_to_digits[sort_string(code)])
     return output_digits
 
 
